75a9f1c94ccdab8f6941f35a8005ba59e0e283bd
Nikola Bulat <nbulat99@gmail.com>
2024-05-19 18:30:15 +0200

    Modify generate_lib.sh

--- MCTS/tsp-20-50-100/generate_lib.sh
+++ MCTS/tsp-20-50-100/generate_lib.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
+
+# Remove existing compiled object file
 rm -r kernel.o 
 
-nvcc -L /usr/local/cuda/lib64/ -I /usr/local/cuda/samples/common/inc -lcudart -lcuda -c kernel.cu
+# Compile the CUDA kernel using the appropriate paths for CUDA 12.2
+nvcc -L /usr/local/cuda-12.2/lib64/ -I /usr/local/cuda-12.2/samples/common/inc -lcudart -lcuda -c kernel.cu
 
 echo "Done."
