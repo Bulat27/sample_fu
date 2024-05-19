3aafd0ce2743743b3a5a8999569ef6b4100b1870
Nikola Bulat <nbulat99@gmail.com>
2024-05-19 12:16:45 +0200

    Change the cuda path in generate_lib.sh

--- MCTS/tsp-20-50-100/generate_lib.sh
+++ MCTS/tsp-20-50-100/generate_lib.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 rm -r kernel.o 
-#rm -r TSP.o
 
-nvcc -L /usr/local/cuda-9.0/lib64/ -I /usr/local/cuda-9.0/samples/common/inc   -lcudart -lcuda  -c kernel.cu
+nvcc -L /usr/local/cuda/lib64/ -I /usr/local/cuda/samples/common/inc -lcudart -lcuda -c kernel.cu
 
 echo "Done."
