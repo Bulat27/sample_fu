152d091012e635f6d840bc46116324a5d1b82d92
Nikola Bulat <nbulat99@gmail.com>
2024-05-22 17:30:49 +0200

    Update the heatmap formatting

--- Att-GraphConvNet/utils/tsplib.py
+++ Att-GraphConvNet/utils/tsplib.py
@@ -37,6 +37,29 @@ def read_tsplib_opt(tsplib_opt_file):
         opt = np.append(opt, 0)
     return opt
 
+# def write_tsplib_prob(tsp_instance_name, edge_prob, num_node, mean, fnn = 0, greater_zero = '---'):
+#     '''
+#     args:
+#         tsp_instance_name : the name of tsplib instances
+#         edge_prob : a symmetric matrix, which inludes edges' probability belonged to the optimization solution
+#         num_node : the number of cities in tsplib instances
+#         fnn : the number of false negative edges
+   
+#     return:
+#         NONE
+#     '''
+#     with open('{}'.format(tsp_instance_name), 'w') as f:
+#         f.write('the mean of prob-rank is {}\n'.format(mean))
+#         f.write('the number of false negetive edges is {}\n'.format(fnn))
+#         f.write('the number of probability greater than 0 is {}/edge\n'.format(greater_zero))
+#         f.write('TYPE: Probability\n')
+#         f.write('DIMENSIOn: {}\n'.format(num_node))
+#         for i in range(num_node):
+#             for j in range(num_node):
+#                 #f.write(' {}'.format(edge_prob[i, j]))
+#                 f.write(' {}'.format('%5f'%edge_prob[i, j]))
+#             f.write('\n')
+
 def write_tsplib_prob(tsp_instance_name, edge_prob, num_node, mean, fnn = 0, greater_zero = '---'):
     '''
     args:
@@ -49,13 +72,15 @@ def write_tsplib_prob(tsp_instance_name, edge_prob, num_node, mean, fnn = 0, gre
         NONE
     '''
     with open('{}'.format(tsp_instance_name), 'w') as f:
-        f.write('the mean of prob-rank is {}\n'.format(mean))
-        f.write('the number of false negetive edges is {}\n'.format(fnn))
-        f.write('the number of probability greater than 0 is {}/edge\n'.format(greater_zero))
-        f.write('TYPE: Probability\n')
-        f.write('DIMENSIOn: {}\n'.format(num_node))
+       
+        f.write('{}\n'.format(num_node))
+        
         for i in range(num_node):
             for j in range(num_node):
                 #f.write(' {}'.format(edge_prob[i, j]))
                 f.write(' {}'.format('%5f'%edge_prob[i, j]))
             f.write('\n')
+
+        f.write('the mean of prob-rank is {}\n'.format(mean))
+        f.write('the number of false negetive edges is {}\n'.format(fnn))
+        f.write('the number of probability greater than 0 is {}/edge\n'.format(greater_zero))
